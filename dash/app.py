d8e7c2d89e7e589bb3fab8742eb36e84ebc5a6d5
thefithagreement <mickael.vitry@gmail.com>
2020-10-01 14:54:34 +0200

    added a slider to select a period

--- dash/app.py
+++ dash/app.py
@@ -2,6 +2,7 @@ import json
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+from dash.dependencies import Input, Output
 import plotly.graph_objects as go
 import pandas as pd
 import boto3
@@ -21,23 +22,11 @@ session = boto3.Session(
     )
 
 s3 = session.client("s3")
-s3.download_file('lead-us-car', 'final/us_car_accident_grouped.csv', './us_car_accident_grouped.csv')
+s3.download_file('lead-us-car', 'final_v2/us_car_accident_grouped.csv', './us_car_accident_grouped.csv')
 
 df = pd.read_csv("./us_car_accident_grouped.csv")
 
-fig = go.Figure(data=go.Choropleth(
-    locations=df['State'], # Spatial coordinates
-    z = df['count'].astype("float"), # Data to be color-coded
-    locationmode = 'USA-states', # set of locations match entries in `locations`
-    autocolorscale=False,
-    colorscale = 'Reds',
-    colorbar_title = "Total car accident",
-))
-
-fig.update_layout(
-    title_text = 'US car accidents distribution by States from 2016 to 2020.',
-    geo_scope='usa', # limite map scope to USA
-)
+# app layout
 
 app.layout = html.Div(children=[
     html.Center(children=[
@@ -51,13 +40,44 @@ app.layout = html.Div(children=[
     html.Div(
         children=[
             dcc.Graph(
-                id='us-car-accident-distribution-graph',
-                figure=fig
+                id='us-car-accident-distribution-graph'
+            ),
+            dcc.Slider(
+                id='year-slider',
+                min=df['year'].min(),
+                max=df['year'].max(),
+                value=df['year'].max(),
+                marks={str(year): str(year) for year in df['year'].unique()},
+                step=None
             )
         ],
         className='section'
     )
 ], className='section')
+
+@app.callback(
+    Output('us-car-accident-distribution-graph', 'figure'),
+    [Input('year-slider', 'value')]
+)
+def update_figure(selected_year):
+    # filtering the data using the chosen year
+    filtered_df = df[df["year"] <= selected_year]
+
+    fig = go.Figure(data=go.Choropleth(
+        locations=df['State'], # Spatial coordinates
+        z = filtered_df['count'].astype("float"), # Data to be color-coded
+        locationmode = 'USA-states', # set of locations match entries in `locations`
+        autocolorscale = False,
+        colorscale = 'Reds',
+        colorbar_title = "Total car accident",
+    ))
+
+    fig.update_layout(
+        title_text = f'US car accidents distribution by States from 2016 to {selected_year}.',
+        geo_scope='usa', # limite map scope to USA
+    )
+
+    return fig
 
 if __name__ == '__main__':
   app.run_server(host='0.0.0.0', debug=True)
